703a737283cb469f0a83f07d116f657edab10a02
VeronikaTapal <143877419+VeronikaTapal@users.noreply.github.com>
2023-10-06 20:10:51 +0300

    Update дз 2, работа 6.py

--- дз 2, работа 6.py
+++ дз 2, работа 6.py
@@ -1,10 +1,7 @@
 user_input = input("Введите строку: ")
 k = int(input("Введите число k: "))
 
-digits = []
-for char in user_input:
-    if char.isdigit():
-        digits.append(char)
+digits = [char for char in user_input if char.isdigit()]
 
 if k > len(digits):
     print("В строке нет", k, "цифры")

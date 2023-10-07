ae5cd9ded70ef27ab7c23e35461f0df921e8ee7e
VeronikaTapal <143877419+VeronikaTapal@users.noreply.github.com>
2023-10-07 13:48:40 +0300

    Update дз 2, работа 2.py

--- дз 2, работа 2.py
+++ дз 2, работа 2.py
@@ -1,11 +1,7 @@
-def largest_number(n):
-	n.sort(reverse=True)
-	num = ''
-	for i in n:
-		num += str(i)
-	return num
-
-num = input('Введите числа: ').split()
-num = [int(i) for i in num]
-
-print(largest_number(num))
+a = input()
+b = []
+while a:
+    b.append(a)
+    a = input()
+b.sort(reverse=True)
+print(''.join(b))

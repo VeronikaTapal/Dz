4ccfd3c1604f08aff4f6b404d08871a41cf71019
VeronikaTapal <143877419+VeronikaTapal@users.noreply.github.com>
2023-10-07 13:29:29 +0300

    Update дз 2, работа 1.py

--- дз 2, работа 1.py
+++ дз 2, работа 1.py
@@ -1,7 +1,7 @@
 elements = []
 while True:
     user_input = input("Введите элемент: ")
-    if user_input == "":
+    if not user_input :
         break
     elements.append(user_input)
 print("Итоговый список:", elements)

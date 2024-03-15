82395ea7e3056dc88f43304b235c3fa78a52b8d0
VeronikaTapal <143877419+VeronikaTapal@users.noreply.github.com>
2024-03-16 01:34:25 +0300

    Update NEWDATES.py

--- new/NEWDATES.py
+++ new/NEWDATES.py
@@ -1,13 +1,11 @@
-from datetime import datetime
-
 class Date:
-    def __init__(self, day, month, year):
+    def __init__(self, year, month, day):
         self.day = day
         self.month = month
         self.year = year
-        self.validate_date()
+        self.validate()
 
-    def validate_date(self):
+    def validate(self):
         if self.day > self.days_in_month():
             raise ValueError("Некорректное количество дней в месяце")
 
@@ -43,48 +41,8 @@ class Date:
         else:
             return "Некорректное значение месяца"
 
-    def __add__(self, other):
-        new_day = self.day + other.day
-        new_month = self.month + other.month
-        new_year = self.year + other.year
-
-        if new_day > self.days_in_month():
-            new_day -= self.days_in_month()
-            new_month += 1
-
-        if new_month > 12:
-            new_month -= 12
-            new_year += 1
-
-        return Date(new_day, new_month, new_year)
-
-    def __sub__(self, other):
-        new_day = self.day - other.day
-        new_month = self.month - other.month
-        new_year = self.year - other.year
-
-        if new_day < 1:
-            new_month -= 1
-            new_day += self.days_in_month()
-
-        if new_month < 1:
-            new_year -= 1
-            new_month += 12
-
-        return Date(new_day, new_month, new_year)
-
 
 class DateStamp(Date):
-    def __init__(self, year, month, day):
-        self.year = year
-        self.month = month
-        self.day = day
-
-    def __str__(self):
-        months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
-                  "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
-        return f"{self.day} {months[self.month-1]} {self.year}"
-
     def __add__(self, other):
         new_day = self.day + other.day
         new_month = self.month + other.month
@@ -127,14 +85,3 @@ class DateStamp(Date):
             new_year -= 1
 
         return DateStamp(new_year, new_month, new_day)
-
-    def days_in_month(self, month, year):
-        if month in [1, 3, 5, 7, 8, 10, 12]:
-            return 31
-        elif month in [4, 6, 9, 11]:
-            return 30
-        else:
-            if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
-                return 29
-            else:
-                return 28
